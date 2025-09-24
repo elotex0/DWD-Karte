4c689954e1189d52f649a61f5e17c83c46b2578b
elotex0 <justin.kettner14@icloud.com>
2025-09-24 13:00:46 +0200

    fixed the end of the script

--- scripts/generate_pngs.py
+++ scripts/generate_pngs.py
@@ -330,6 +330,6 @@ footer_ax.text(0.99, 0.68, f"{valid_time_local:%d.%m.%Y %H:%M} Uhr",
                fontsize=12, va="top", ha="right", fontweight="bold")
 
 # Speichern
-    outname = f"{var_type}_{valid_time_local:%Y%m%d_%H%M}.png"
-    plt.savefig(os.path.join(output_dir, outname), dpi=100, bbox_inches=None, pad_inches=0)
-    plt.close()
+outname = f"{var_type}_{valid_time_local:%Y%m%d_%H%M}.png"
+plt.savefig(os.path.join(output_dir, outname), dpi=100, bbox_inches=None, pad_inches=0)
+plt.close()
